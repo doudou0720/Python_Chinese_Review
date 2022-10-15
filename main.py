13ef8b63a6be1badbd4662e87d681aa505a0f142
doudou0720 <98651603+doudou0720@users.noreply.github.com>
2022-10-15 14:02:43 +0800

    Dev : Bulid number 0003

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # 导入Flask类库
 from flask import Flask , render_template
+import uuid
 # 创建应用实例
 app = Flask(__name__,template_folder="./templates",static_folder="./static")
+def create_uuid():
+	return uuid.uuid4().hex
 # 视图函数（路由）
 @app.route('/')
 def index():
 	return render_template("loading.html")
 # 启动实施（只在当前模块运行）
+
 if __name__ == '__main__':
 	app.run(debug=True,port=80,host="0.0.0.0")
