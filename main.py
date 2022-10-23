3c137a86213b6cc0d63130ebcb7b2d54425f7b5e
doudou0720 <961863600@qq.com>
2022-10-23 11:09:32 +0800

    Dev : Build number 0006

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # 导入Flask类库
-from flask import Flask , render_template
+from flask import Flask , render_template , request
 import uuid
 # 创建应用实例
 app = Flask(__name__,template_folder="./templates",static_folder="./static")
@@ -8,8 +8,11 @@ def create_uuid():
 # 视图函数（路由）
 @app.route('/test/home')
 def index():
-	return render_template("home.html")
+	return render_template("home.html",user_name = "test")
 # 启动实施（只在当前模块运行）
-
+@app.route('/deal')
+def deal():
+	type = request.args.get("type")
+	return "OK"
 if __name__ == '__main__':
 	app.run(debug=True,port=80,host="0.0.0.0")
