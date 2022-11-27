a155cbed2cf4407df4fd8d70317ae8574b35c6e6
doudou0720 <98651603+doudou0720@users.noreply.github.com>
2022-11-27 12:38:15 +0800

    Dev : Bulid number 0008

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# 导入Flask类库
 import uuid , os
 from flask import Flask , render_template , request
 # 创建应用实例
@@ -7,13 +6,22 @@ app = Flask(__name__,template_folder="./templates",static_folder="./static")
 def create_uuid():
     return uuid.uuid4().hex
 # 视图函数（路由）
-@app.route('/test/home')
+@app.route('/home')#主页
 def index():
     return render_template("home.html",user_name = "test")
-# 启动实施（只在当前模块运行）
-@app.route('/deal')
-def deal():
-    type = request.args.get("type")
+@app.route('/about')#关于
+def about():
+    return render_template("about.html")
+@app.route('/history')#历史
+def history():
+    return render_template("history.html",historys=[{"title":"Test","description":"Test,too","id":114514},{"title":"Test2","description":"Test2,too","id":5201314}])
+@app.route('/whiledoing')#任务进行时
+def while_doing():
+    return render_template("whiledoing.html")
+@app.route('/get_task_json')#后台:获取任务json
+def get_task_json():
+    id = request.args.get("id")
     return "OK"
+# 启动实施（只在当前模块运行）
 if __name__ == '__main__':
-    app.run(port=80,host="0.0.0.0")
+    app.run(debug=True,port=80,host="0.0.0.0")
