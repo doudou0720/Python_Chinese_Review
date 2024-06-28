563c286ea8f2962e8815e3e6b61d20943032511b
doudou0720 <98651603+doudou0720@users.noreply.github.com>
2024-06-28 16:59:12 +0800

    [DEV] June 28,2024 , 1st commit
    Dict part 1 (Fix)

--- extensions/main/english/dictionary.py
+++ extensions/main/english/dictionary.py
@@ -1,5 +1,7 @@
 import logging
+import progressbar
 from flask import Flask,render_template,request,url_for,redirect
+import requests
 from . import stardict as sd
 import os
 
@@ -8,51 +10,25 @@ run_dir = ""
 json_info = {}
 # from run import app
 app = Flask
-#多线程下载部分摘自https://blog.csdn.net/as604049322/article/details/119847193
-import asyncio
-import httpx
-import requests
+import urllib.request
 
+def download_files(url,name):
+    response = requests.request("GET", url, stream=True, data=None, headers=None)
 
-def calc_divisional_range(filesize, chuck=10):
-    step = filesize//chuck
-    arr = list(range(0, filesize, step))
-    result = []
-    for i in range(len(arr)-1):
-        s_pos, e_pos = arr[i], arr[i+1]-1
-        result.append([s_pos, e_pos])
-    result[-1][-1] = filesize-1
-    return result
+    save_path = "./extensions/main/english/"+name
 
-
-
-def download_files(url,save_name):
-    # 下载方法
-    async def async_range_download(save_name, s_pos, e_pos):
-        headers = {"Range": f"bytes={s_pos}-{e_pos}"}
-        extension_logger.info("Trying to get:{url}:{}")
-        res = await client.get(url, headers=headers)
-        
-        with open(save_name, "rb+") as f:
-            f.seek(s_pos)
-            f.write(res.content)
-    client = httpx.AsyncClient(timeout=None)
-
-    res = httpx.head(url)
-    filesize = int(res.headers['Content-Length'])
-    divisional_ranges = calc_divisional_range(filesize, 20)
-
-
-    # 先创建空文件
-    with open(save_name, "wb") as f:
-        pass
-
-    loop = asyncio.get_event_loop()
-    tasks = [async_range_download(save_name, s_pos, e_pos)
-            for s_pos, e_pos in divisional_ranges]
-    # 等待所有协程执行完毕
-    loop.run_until_complete(asyncio.wait(tasks))
-
+    total_length = int(response.headers.get("Content-Length"))
+    with open(save_path, 'wb') as f:
+        widgets = ['Progress: ', progressbar.Percentage(), ' ',
+            progressbar.Bar(marker='#', left='[', right=']'),
+            ' ', progressbar.ETA(), ' ', progressbar.FileTransferSpeed()]
+        pbar = progressbar.ProgressBar(widgets=widgets, maxval=total_length).start()
+        for chunk in response.iter_content(chunk_size=1):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+            pbar.update(len(chunk) + 1)
+        pbar.finish()
 
 cl = r'.\dict_l.db'
 def init(k:Flask,ext_logger:logging.Logger,run_d:str):
@@ -66,8 +42,15 @@ def init(k:Flask,ext_logger:logging.Logger,run_d:str):
         extension_logger.info("[For countries that cannot connect to Github]如果下载速度过慢，你可以尝试手动下载，保存到{path}/temp_dict下".format(path=run_d))
         cnt = 0
         while True:
-            if cnt >= 5:
-                extension_logger.error("Cannot Download Files!The dictionary part WILL NOT be loaded")
+            if cnt > 1:
+                if cnt < 4:
+                    try :
+                        extension_logger.info("Try to download it from https://github.moeyy.xyz/ ...")
+                        download_files("https://github.moeyy.xyz/https://github.com/skywind3000/ECDICT/releases/download/1.0.28/ecdict-sqlite-28.zip","ecdict-sqlite-28.zip")
+                    except Exception as e:
+                        extension_logger.error("The following error occurred on the {k}th download attempt(Use moeyy):\n{e}".format(k=cnt+1,e=e))
+                else :
+                    extension_logger.error("Cannot Download Files!The dictionary part WILL NOT be loaded")
                 return
             try:
                 download_files("https://github.com/skywind3000/ECDICT/releases/download/1.0.28/ecdict-sqlite-28.zip","ecdict-sqlite-28.zip")
