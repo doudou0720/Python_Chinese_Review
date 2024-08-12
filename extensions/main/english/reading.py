20875c23d35e8c78cc2d1149ce8fb351fe84401c
doudou0720 <98651603+doudou0720@users.noreply.github.com>
2024-08-12 14:27:25 +0800

    [DEV] Aug 12th,2024 , 1st commit
    [A]dd Fonts(LXGW)
    [A]dd study route
    [M] Format some files

--- extensions/main/english/reading.py
+++ extensions/main/english/reading.py
@@ -1,5 +1,5 @@
-from flask import render_template,request,Flask,Response
-import os,re,logging,json
+from flask import render_template, request, Flask, Response
+import os, re, logging, json
 
 import requests
 
@@ -11,35 +11,46 @@ json_info = {}
 # from run import app
 app = Flask
 
-def init(k:Flask,ext_logger:logging.Logger,run_d:str):
-    global app ,extension_logger ,run_dir
+
+def init(k: Flask, ext_logger: logging.Logger, run_d: str):
+    global app, extension_logger, run_dir
     app = k
     extension_logger = ext_logger
     run_dir = run_d
+
     @app.route("/extension/main.basic.english.reading/show_articles")
     def show_articles():
         concert = request.args.get("id")
-        with open(f"{os.getcwd()}/data/Articles/English/{concert}.txt","r") as f:
-            a = (f.read())
+        with open(f"{os.getcwd()}/data/Articles/English/{concert}.txt", "r") as f:
+            a = f.read()
             b = []
-            a = re.sub(letters,"",a)
+            a = re.sub(letters, "", a)
             # print(a)
             a = a.split("\n")
             for i in a:
                 b.append(i.split(" "))
-            return render_template("extensions/main/english/reading/reading.html",a = b)
+            return render_template("extensions/main/english/reading/reading.html", a=b)
+
     @app.route("/extension/main.basic.english.reading/append_word")
     def append_word():
         concert = request.args.get("word")
         if concert == None:
-            return Response("Cannot Get key 'word'",400)
-        concert = requests.get("http://127.0.0.1:5000/extension/main.basic.english.dictionary/get/root?word="+concert)
+            return Response("Cannot Get key 'word'", 400)
+        concert = requests.get(
+            "http://127.0.0.1:5000/extension/main.basic.english.dictionary/get/root?word="
+            + concert
+        )
         if concert.status_code == 200:
             pass
-            return Response("Append successfully!",200)
+            return Response("Append successfully!", 200)
         else:
-            return Response("Get root word from '/extension/main.basic.english.dictionary/get/root' failed",500)
+            return Response(
+                "Get root word from '/extension/main.basic.english.dictionary/get/root' failed",
+                500,
+            )
+
     extension_logger.info("Load main.basic.english.reading successfully!")
 
+
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
